d99fa8f17ffa17dc7f4c777b039e6ea9e9de5032
OnlineStudentHere <158177485+OnlineStudentHere@users.noreply.github.com>
2024-02-13 05:46:24 -0800

    new JavaScript project added
    new JavaScript project added

---
+++ Day7-ClassWork/day7-javascript-classwork.js
@@ -0,0 +1,49 @@
+
+
+/*created by online student .ie*/
+
+
+
+
+// TASK 1
+function some_person () {
+
+    // 1 const
+     const firstName = "John";
+     const lastName = "Doe";
+      
+     let fullName = firstName + " " + lastName;
+     console.log(fullName); 
+     
+     // 2 Array
+     const person1 = ['John', 'Doe'];
+     console.log(person1); 
+     
+     //const person2 = {'John', 'Doe' };
+     //console.log(person1); 
+     
+     // 3 Object
+     const person2 = {
+ 
+         firsname:"John",
+          lastname:"Doe"
+     
+         };
+     console.log(person2);
+ };
+ some_person();
+ 
+ 
+ 
+ // TASK 2
+ const school_current_name = 'Programming Coding School';
+ const school_new_name = school_current_name.replace('Coding',"CODING");
+ console.log(school_new_name);
+ 
+ 
+ //TASK 3
+ 
+ const reverse_school_name_1 = school_current_name.replace('Programming Coding School',"School Coding Programming");
+ console.log(reverse_school_name_1);
+ 
+ 
